2499cb68d8de5d009ce5e285f2ff55fe00683895
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-02-17 13:36:02 -0800

    g

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - deader
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != q && ch != e)
+		{
+			putchar(ch);
+		}
+	}
+	return (0);
+}
