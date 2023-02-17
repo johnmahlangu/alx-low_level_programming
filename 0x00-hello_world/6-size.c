2d41b21d786a809f38ce89030a765658e4a5e94f
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-02-16 19:07:50 -0800

    o

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	str byte = "byte(s)";
+	char byte[] = "byte(s)";
 
-	printf("Size of a char: ", sizeof(char), " ", byte;
-	printf("Size of a int: ", sizeof(int), " ", byte;
-	printf("Size of a long int: ", sizeof(long int), " ", byte;
-	printf("Size of a long long int: ", sizeof(long long int), " ", byte;
-	printf("Size of a float: ", sizeof(float), " ", byte;
+	printf("Size of a char: ", sizeof(char), " ", byte);
+	printf("Size of a int: ", sizeof(int), " ", byte);
+	printf("Size of a long int: ", sizeof(long int), " ", byte);
+	printf("Size of a long long int: ", sizeof(long long int), " ", byte);
+	printf("Size of a float: ", sizeof(float), " ", byte);
 	return (0);
 }
