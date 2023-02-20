9c40a5ec036b9775d9ba690f79ea5d16d0ffbd56
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-02-20 05:04:29 -0800

    j

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - header
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (k = 0; k <= '9'; k++)
+		{
+			putchar('i');
+			putchar('k');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
