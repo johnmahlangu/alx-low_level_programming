7965d51b6bca2392463c3a942fa6025faad97bc8
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-02-21 08:01:33 -0800

    o

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.c"
+/**
+ * main - header
+ *
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++;)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
