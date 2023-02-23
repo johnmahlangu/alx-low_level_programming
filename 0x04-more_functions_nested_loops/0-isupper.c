bcd3258125daf5c3adccfb811a43df054b5a72c6
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-02-23 15:25:16 -0800

    l

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/**_isupper - prints whether a character is upper case or not
+/**
+ * _isupper - prints whether a character is upper case or not
  * @c: variable
  * Return: 1 if uppercase or 0 otherwise
  */
