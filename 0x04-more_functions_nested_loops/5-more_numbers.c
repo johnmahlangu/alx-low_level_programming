e69130237e4cbc93ff661be16dc99aecf3be029a
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-02-23 16:24:06 -0800

    m

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i = '0', k;
+
+	while (i <= '9')
+	{
+		k = '0';
+		while (k <= '13')
+		{
+			_putchar(i);
+			_putchar(k);
+			k++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
