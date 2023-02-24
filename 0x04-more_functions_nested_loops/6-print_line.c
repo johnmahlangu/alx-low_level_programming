a7f7327a00d2cea24a839125c0eb5d5c69a71035
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-02-24 14:06:17 -0800

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - print lines
+ * @n: number of lines
+ * Return: number of lines
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			char j = _putchar('_');
+		}
+	}
+	return (j);
+}
