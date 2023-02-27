09cb02fe705bed5652c320a3b7f9f28d38080032
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-02-27 12:26:36 -0800

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - length of string
+ * @s: length of a string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	s[1000];
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (i + '\0');
+}
+
