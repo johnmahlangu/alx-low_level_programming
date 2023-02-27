f07d55295fdfe0fe8cefc31ae2a9809b5719029f
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-02-27 14:20:12 -0800

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - print in reverse
+ * @s: variable
+ */
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\n'; i++)
+	{
+
+	}
+
+	for (j = i; j >= 0; j--)
+		_putchar(s[j]);
+
+	_putchar('\n');
+}
