2cad6b2642db5d2b46242943d29635e6aa890b65
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-01 07:53:27 -0800

    m

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,29 @@ void print_to_98(int n)
 	int i;
 	int last = 98;
 
-	for (i = n; i <= 98; i++)
+	if (n > 98)
 	{
-		if (i != 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ",i);
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+				printf("%d", last);
 		}
-		else
-			printf("%d",last);
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+				printf("%d", last);
+		}
 	}
 	printf("\n");
 }
