9217549edfb2f816b7c4ee078fc250722b16952d
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-01 03:00:21 -0800

    l

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * print_last_digit: print last digit 
+ * print_last_digit - print last digit
  * @c: number
  * Return: 0
  */
 int print_last_digit(int c)
 {
 	int i = c % 10;
+
 	if (i < 0)
 	{
 		i = i * -1;
