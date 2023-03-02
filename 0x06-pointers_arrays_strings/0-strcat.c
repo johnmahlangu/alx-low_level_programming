91516f1a7be858b7e44863c1ae6eed2ec449e39b
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-02 11:02:26 -0800

    l

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while(dest[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
@@ -22,6 +22,5 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
