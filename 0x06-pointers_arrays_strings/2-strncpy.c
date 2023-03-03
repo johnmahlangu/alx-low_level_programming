47d31afbc9d2380d787dec25e426bc10512084ed
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-03 03:45:06 -0800

    m

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	
+
 	j = 0;
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while(i < n && src[i] != '\0')
 	{
-		dest[j] = src[i];
+		dest[j] = src[i]
 		i++;
 		j++;
 	}
