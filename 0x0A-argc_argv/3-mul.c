e5893e4fc44b3052539be88451994db4fea0543e
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-10 15:14:03 -0800

    j

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main funtion
+ * @argc: int
+ * @argv: chara
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc == 0 || argc == 1)
+	{
+		printf("Error");
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (i > 0)
+			{
+				mul *= atoi(argv[i]);
+			}
+		}
+	}
+	printf("%d\n", mul);
+	return (0);
+}
