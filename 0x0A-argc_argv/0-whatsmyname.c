b14d438a7c2b3359ac35c1a8cb7927b22c4e2141
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-10 13:22:41 -0800

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
  * main - prog prints its name
+ * @argc: count
+ * @argv: string
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	
-	printf("%s\n",argv[i]);	
+
+	printf("%s\n", argv[i]);
 	return (0);
 }
