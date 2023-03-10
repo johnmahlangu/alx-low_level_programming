42eafedb4d6482f8cbea49c71e5ad98e5e411f26
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-10 14:16:10 -0800

    i

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - num arg
+ * @argc: string
+ * @argv: whatever
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	(void) argv;
+
+	for (i = 0; i < argc - 1; i++)
+	{
+
+	}
+
+	printf("%d\n", i);
+	return (0);
+}
