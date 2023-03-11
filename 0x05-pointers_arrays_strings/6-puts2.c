82e104dd099d5efe545bf6e3e9c6dfdab9a114c7
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-11 06:10:58 -0800

    m

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - print every other character
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
