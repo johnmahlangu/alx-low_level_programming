a1c8372428ed1dad855935863d667b21ba42eadc
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-15 14:29:58 -0700

    m

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concat string
+ * @s1: string 1
+ * @s2; string 2
+ * Return: concatenated
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int a = 0;
+	int b = 0;
+	char *c;
+
+	if (s1 == NULL)
+		return (NULL);
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+
+	if (s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	k = i + j;
+	c = malloc((sizeof(char) * k) + 1);
+
+	while (a < i)
+	{
+		c[a] = s1[a];
+		a++;
+	}
+	while (b < k)
+	{
+		c[i] = s2[b];
+		b++;
+		i++;
+	}
+	c[k] = '\0';
+	return (c);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,4 @@
 #pragma once
 int _putchar(char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
