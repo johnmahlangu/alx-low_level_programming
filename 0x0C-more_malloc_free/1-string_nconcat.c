97d2b8137eeecf59fee9b26274a88e60b37867a2
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-16 16:02:52 -0700

    n

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenate
+ * @s1: string 1
+ * @s2: string 2
+ * @n: length
+ * Return: concatenated
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0;
+	int j = 0;
+	unsigned int k = 0;
+	char *c;
+
+	if (s1 == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	c = malloc(sizeof(char) * (n + i + 1));
+	while (j < i)
+	{
+		c[j] = s1[j];
+		j++;
+	}
+	if (s2 == NULL)
+		return (NULL);
+	while (k < n)
+	{
+		c[i] = s2[k];
+		i++;
+		k++;
+	}
+	c[i] = '\0';
+	return (c);
+}
