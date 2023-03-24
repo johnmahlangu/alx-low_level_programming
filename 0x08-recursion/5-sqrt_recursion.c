a0692da07c3e803371870ef16acb40b6be683ee9
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-24 08:21:11 -0700

    m

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
+/**
+ * _sqrt_recursion - ...
+ * @n: ...
+ * Return: ..
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+
+	if (sqrt == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
+}
