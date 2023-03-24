d96f290c945cd6e36229bcecd69600b3cce2c160
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-03-24 14:32:17 -0700

    m

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,15 +19,14 @@ int is_prime_number(int n)
  */
 int check_prime(int n, int i)
 {
-    if (n <= 1)
-        return 0;
+	if (n <= 1)
+		return (0);
 
-    if (n % i == 0 && i > 1)
-        return 0;
+	if (n % i == 0 && i > 1)
+		return (0);
 
-    if (i * i > n)
-        return 1;
+	if (i * i > n)
+		return (1);
 
-    return check_prime(n, i + 1);
+	return (check_prime(n, i + 1));
 }
-
