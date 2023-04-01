d04a2a2c14b6a72283717f2f198dd616f0bec200
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-04-01 07:41:52 -0700

    m

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,10 +21,5 @@ struct dog
  * @age: ...
  * @owner: ...
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /*DOG_H*/
