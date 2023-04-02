6c207beafe7d7efdf60cb1d3e805d8236b09f38c
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-04-02 13:49:26 -0700

    m

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - ...
+ * @h: ...
+ * Return: ...
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
