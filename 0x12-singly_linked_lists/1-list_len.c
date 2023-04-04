35536dfe79a95d61107145b33460b2872eb4a5aa
Jonas Mahlangu <jonasthokozani2000@gmailcom>
2023-04-04 06:29:07 -0700

    m

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include  <stdio.h>
+/**
+ * list_len - ...
+ *@h: ...
+ *Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h)
+	{
+		i++;
+		h->next;
+	}
+	return (i);
+}
