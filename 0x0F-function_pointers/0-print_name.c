b5d3bce888f52df908f790677a81766ee147e952
Jonas Mahlangu <jonasthokozani2000@gmail.com>
2023-04-19 12:04:44 -0700

    mj

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - print name
@@ -8,6 +9,6 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-		return NULL;
+		return;
 	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 #ifndef FUNC_POINT
 #define FUNC_POINT
+#include <stdio.h>
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
