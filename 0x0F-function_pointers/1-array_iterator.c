df135e57f8c5af2ca2f8836f3ce37eed61fb62ba
Jonas Mahlangu <jonasthokozani2000@gmail.com>
2023-04-19 11:49:30 -0700

    j

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate an array
+ * @array: ...
+ * @size: ...
+ * @action: ...
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
