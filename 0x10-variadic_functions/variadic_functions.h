61fccfbfbb8c093364c88d5cd652a1b5563340e9
Jonas Mahlangu <jonasthokozani2000@gmail.com>
2023-04-21 16:16:26 -0700

    jgy

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 int _putchar(char c);
+void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
