4d1aeecfbbcb9ad928c8bfdef435c86aac6d750f
Jonas Mahlangu <jonasthokozani2000@gamil.com>
2023-04-28 14:25:11 -0700

    adl

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,6 +20,10 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
 
 	newnode->str = strdup(str);
 	newnode->len = i;
