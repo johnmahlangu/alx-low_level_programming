38562a4331c0b3f48b9f4b01017571eef0d7bc10
Jonas Mahlangu <jonasthokozani2000@gamil.com>
2023-04-28 14:13:31 -0700

    lkok

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_node - ...
+ * @head: ...
+ * @str: ...
+ * Return: address of newnode
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+	int i = 0;
+
+	while (str != NULL)
+	{
+		i++;
+	}
+
+	newnode = malloc(sizeof(list_t));
+
+	newnode->str = strdup(str);
+	newnode->len = i;
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+	}
+	else
+	{
+		newnode->next = *head;
+		*head = newnode;
+	}
+
+	return (newnode);
+}
